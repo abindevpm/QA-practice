004ab7e52b60a81f7cdc99a6c5d46c3e5c6c4611
abindevpm <abindevpm6@gmail.com>
2026-07-09 05:25:51 +0530

    http server

--- node.js
+++ node.js
@@ -1,9 +1,26 @@
-const express = require("express")
-const app = express()
-app.get("/",(req,res)=>{
-    res.send("this is express server")
-})
+  // express server
 
-app.listen(3000,()=>{
-    console.log('server is running on 3000')
-})
+
+// const express = require("express")
+// const app = express()
+// app.get("/",(req,res)=>{
+//     res.send("this is express server")
+// })
+
+// app.listen(3000,()=>{
+//     console.log('server is running on 3000')
+// })
+
+ 
+//  http server
+
+// const http = require("http")
+//  const server =  http.createServer((req,res)=>{
+//     res.write("this is http server")
+//     res.end()
+// })
+
+// server.listen(3000,()=>{
+//     console.log("server is running on 3000")
+// })
+
