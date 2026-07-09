15fddcf2a5a0d51bb1da838b938dc869dcae5890
abindevpm <abindevpm6@gmail.com>
2026-07-09 06:20:17 +0530

    Error handling middleware

--- node.js
+++ node.js
@@ -42,3 +42,24 @@
 // app.listen(3000,()=>{
 //     console.log('server is running on 3000')
 // })
+
+
+
+// Error handling middleware
+
+
+// const express = require("express")
+// const app = express()
+
+// app.get("/",(req,res)=>{
+//     throw new Error("something went wrong")
+// })
+
+// app.use((err,req,res,next)=>{
+//     console.log(err.message)
+//     res.status(500).send("Internal Server Error")
+// })
+
+// app.listen(3000,()=>{
+//     console.log("server is running on 3000")
+// })
