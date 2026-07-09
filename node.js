4d3c5b2f2a1555642fcafe4d727a335f23eda785
abindevpm <abindevpm6@gmail.com>
2026-07-09 05:39:17 +0530

    application level middleware

--- node.js
+++ node.js
@@ -24,3 +24,21 @@
 //     console.log("server is running on 3000")
 // })
 
+
+//   application level middleware
+
+//      const express = require("express")
+// const app = express()
+
+// app.use((req,res,next)=>{
+//     console.log("this is application level middleware")
+//     next()
+// })
+
+// app.get('/',(req,res)=>{
+//     res.send("this is server")
+// })
+
+// app.listen(3000,()=>{
+//     console.log('server is running on 3000')
+// })
