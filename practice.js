8006b0f61513b968e24ab636ba87c1e1db1d317a
abindevpm <abindevpm6@gmail.com>
2026-07-14 13:43:59 +0530

    promise

--- practice.js
+++ practice.js
@@ -1,4 +1,4 @@
-// callback 
+                     // callback 
 
 // function a(name,callback){
 //     console.log('hy'+name)
@@ -11,3 +11,26 @@
 // }
 
 // a("abindev",b)
+
+
+                      // Promise 
+
+//  let a = new Promise((resolve,reject)=>{
+//     let i = true
+//     if(i === true){
+//         resolve("this is promise")
+//     }else{
+//         reject("not a  promise")
+//     }
+    
+// })
+
+// .then((success)=>{
+//     console.log(success)
+// })
+
+// .catch((error)=>{
+//     console.log(error)
+// })
+
+
