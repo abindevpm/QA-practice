067a9cf28a6fc2675a9a33310e40d8c9179d07a5
abindevpm <abindevpm6@gmail.com>
2026-07-14 16:43:29 +0530

    find unique element in array

--- practice.js
+++ practice.js
@@ -63,4 +63,26 @@
 //       let a = null
 //  let b = a ?? "abindev"
  
-//   console.log(b)             
+//   console.log(b)            
+
+
+                  // find unique element in array
+
+
+//    let a = [1,2,3,4,1,2,3,4,5]
+ 
+//  let uniq = []
+ 
+//   for(let i = 0;i<a.length;i++){
+//       let count = 0;
+//       for(let j = 0;j<a.length;j++){
+//           if(a[i] === a[j]){
+//               count++
+//           }
+//       }
+//       if(count === 1){
+//           uniq[uniq.length] = a[i]
+//       }
+//   }
+  
+//    console.log(uniq)                
