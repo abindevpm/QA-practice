8b37223f5a8bfe91488d628360975c15d680f784
abindevpm <abindevpm6@gmail.com>
2026-07-15 10:46:26 +0530

    reverse a strring

--- practice.js
+++ practice.js
@@ -212,3 +212,15 @@
 //  }
 
 // console.log(sum)                    
+
+                    // reverse a string
+
+// let a = [1,2,3,4,5];
+
+// for(let i = 0;i<a.length/2;i++){
+//     let temp = a[i]
+//     a[i] = a[a.length-1-i]
+//     a[a.length-1-i] = temp
+// }
+
+// console.log(a)                    
