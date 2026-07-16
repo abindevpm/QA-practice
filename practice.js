482b0d1c01ae31f238b632e2ee3f4303bc9ee397
abindevpm <abindevpm6@gmail.com>
2026-07-16 21:19:59 +0530

    missing number finding

--- practice.js
+++ practice.js
@@ -397,4 +397,15 @@
   
 //   console.log(a)
                       
+
+                      //  Find Missing Number in array
+
+//  let a = [1, 2,3,5];
+
+// for(let i = 0;i<a.length;i++){
+//     if(a[i+1]!==a[i]+1){
+//         console.log(a[i]+1)
+//         break
+//     }
+// }                     
                      
